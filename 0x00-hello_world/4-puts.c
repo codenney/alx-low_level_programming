f6711485bdae0f8424dce4f4a08d3b95996dcabf
Olaitan Adedokun <kenney9224@gmail.com>
2022-03-10 06:51:37 -0800

    A C program that prints using puts followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Value 0 as success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
