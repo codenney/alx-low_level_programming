88aa9ed8b9f58fd9d81a435e55bf98118767eff1
Olaitan Adedokun <kenney9224@gmail.com>
2022-03-11 22:32:46 +0100

    Add the conventional styles to it

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
+ *
+ * Return: always 0 as success
  */
 int main(void)
 {
 	char c;
 
-	for(c = 'a'; c <= 'z'; c++)
-	  putchar(c);
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
 	putchar('\n');
 
 	return (0);
