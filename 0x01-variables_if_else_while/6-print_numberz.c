26b5f4aaf54b17c3b172012d411678ab81b3329f
Olaitan Adedokun <kenney9224@gmail.com>
2022-03-11 23:51:55 +0100

    A program that prints base 10 numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	int num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
