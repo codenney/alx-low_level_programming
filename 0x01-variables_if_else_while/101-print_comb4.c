75ac9f6502459895b3f9e13f05e35d869f78ff11
Olaitan Adedokun <kenney9224@gmail.com>
2022-03-12 05:35:50 +0100

    First submission

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description: A program that prints all possible
+ * ...combinations of three digits
+ *
+ * Return: 0 as success
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	i = 0;
+
+	while(i < 1000)
+	{
+		j = i / 100;
+		k = (i / 10) % 10;
+		l = i % 10;
+	}
+
+	return (0);
+}
