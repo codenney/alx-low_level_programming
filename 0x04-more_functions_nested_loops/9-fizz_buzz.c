5f23d0f8ca6e00ecf65120e11e99a30107624b26
Olaitan Adedokun <kenney9224@gmail.com>
2022-03-17 20:32:44 +0100

    Optimize the spacing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,17 +15,20 @@ int main(void)
 	while (num <= 100)
 	{
 		if ((num % 5 == 0) && (num % 3 == 0))
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (num % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (num % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
 			printf("%d ", num);
 
+		if (num < 100)
+			printf(" ");
+
 		num++;
 	}
-
+	
 	printf("\n");
 
 	return (0);
