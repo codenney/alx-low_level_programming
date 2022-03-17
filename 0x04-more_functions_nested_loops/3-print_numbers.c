0f2fea3b2c2164abbb60acc333be7f44c9551ead
Olaitan Adedokun <kenney9224@gmail.com>
2022-03-17 17:26:47 +0100

    Follow Betty convention

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * mul - Function entry
+ * print_numbers - Function entry
  * Description: A function that prints the numbers,
  * ...from 0 to 9, followed by a new line
  * Return: nothing is bliss!
  */
 void print_numbers(void)
 {
-    int num = 0;
+	int num = 0;
 
-    while (num < 10)
-    {
-        _putchar(num + '0');
-        num++;
-    }
+	while (num < 10)
+	{
+		_putchar(num + '0');
+		num++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
