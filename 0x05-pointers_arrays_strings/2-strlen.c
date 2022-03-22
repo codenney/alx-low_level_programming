7d008fced5e860dfa77c6fad29efd5992f018dcd
Olaitan Adedokun <kenney9224@gmail.com>
2022-03-22 04:12:16 +0100

    A function that returns the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,15 +8,14 @@
  */
 int _strlen(char *s)
 {
-int i = 0;
-int length = 0;
+	int i = 0;
+	int length = 0;
 
-while (s[i] != '\0')
-{
-length += 1;
-i++;
-}
+	while (s[i] != '\0')
+	{
+		length += 1;
+		i++;
+	}
 
-return (length);
-
+	return (length);
 }
