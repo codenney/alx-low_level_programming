5630db7dc42cc06a8f92ac62154434d53a540b21
Olaitan Adedokun <kenney9224@gmail.com>
2022-04-02 02:51:32 +0100

    Return 0 if successful

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * Description: A program that prints its name, followed by a new line
  * @argc: the argument count
  * @argv: the array of character pointers
- * Return: See nothing!
+ * Return: 0 if successful
  */
 void main(int __attribute__((__unused__)) argc, char **argv)
 {
@@ -13,5 +13,5 @@ void main(int __attribute__((__unused__)) argc, char **argv)
 
 	printf("%s\n", myName);
 
-	/*return (0);*/
+	return (0);
 }
