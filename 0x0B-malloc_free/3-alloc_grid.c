1de3def5c2894722af6b35c7e3f60f872782ac17
Olaitan Adedokun <kenney9224@gmail.com>
2022-04-06 05:15:51 +0100

    A function that create memory for a 2 dimensional array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Function entry
+ * Description: A function that create memory
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: a pointer to the 2 dimensional array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width + height < 2 || width < 1 || height < 1)
+		return (NULL);
+
+	grid = malloc(height * sizeof(*grid));
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(**grid));
+		if (grid[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
+}
