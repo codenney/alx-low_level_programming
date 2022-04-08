d572572a6b7e5d3d3f8f530caeda9b2ad2448071
Olaitan Adedokun <kenney9224@gmail.com>
2022-04-08 03:48:16 +0100

    A change in the malloc size

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	
-	space = (int *)malloc(sizeof(int) * size);
+	space = (int *)malloc(sizeof(space) * size);
 
 	if (space == NULL)
 		return (NULL);
