4f8f92f300d236e8338f2ea379cccba7d21ad1a2
Olaitan Adedokun <kenney9224@gmail.com>
2022-04-12 00:14:09 +0100

    Add the struct documentation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - A dog struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Define a new type struct dog
+ */
 struct dog
 {
 	char *name;
