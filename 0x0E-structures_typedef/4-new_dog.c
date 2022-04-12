dd1cd8efd355790cc2daa39a4270542e1b3c32b6
Olaitan Adedokun <kenney9224@gmail.com>
2022-04-12 05:55:08 +0100

    Free allocated space if unsuccessful

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+int _strlen(char *s);
 /**
  * new_dog - Function entry
  * Description: A function that creates a new dog
@@ -17,18 +17,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	my_dog = malloc(sizeof(*my_dog));
 
 	if (my_dog == NULL || !(name) || age < 0 || !(owner))
+	{
+		free(my_dog);
 		return (NULL);
+	}
+
 	(*my_dog).age = age;
 
-	while (name[name_len] != '\0')
-		name_len++;
-	while (owner[owner_len] != '\0')
-		owner_len++;
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
+
 	(*my_dog).name = malloc(name_len + 1);
 	(*my_dog).owner = malloc(owner_len + 1);
 
 	if ((*my_dog).name == NULL || (*my_dog).owner == NULL)
+	{
+		free(my_dog);
+		free((*my_dog).name);
+		free((*my_dog).owner);
 		return (NULL);
+	}
 
 	while (i < name_len)
 	{
@@ -47,4 +55,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 	(*my_dog).owner[i] = '\0';
 
 	return (my_dog);
+}
+
+
+/**
+ * _strlen - Entry point
+ * Description - A function that returns the length of a string
+ * *@s: the function accepts an input saved into s
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	int length = 0;
+
+	while (s[i] != '\0')
+	{
+		length += 1;
+		i++;
+	}
+
+	return (length);
 }
