421d6b927d8882044cdab964d9e803ddbaa67944
Olaitan Adedokun <kenney9224@gmail.com>
2022-04-12 01:23:27 +0100

    A function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Function entry
+ * Description: A function that frees dogs
+ * @d: The struct data type
+ * Return: See nothing!
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		free(d);
+}
