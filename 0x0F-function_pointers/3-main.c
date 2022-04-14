702a25e5c1e72ff715219dd8389914ad709d6d01
Olaitan Adedokun <kenney9224@gmail.com>
2022-04-14 05:34:11 +0100

    A program to perform simple arithmetics

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+
+/**
+ * main - Main entry
+ * Description: A program to perform arithmetics
+ * @argc: commandline count
+ * @argv: commandline argument(s)
+ * Return: 0 as success
+ */
+int main(int argc, char **argv)
+{
+	int a, b, result;
+	char *sign = argv[2];
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(sign) == NULL || sign[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(*(argv + 1));
+	b = atoi(*(argv + 3));
+
+	if ((*(sign + 0) == '/' || *(sign + 0) == '%') && (a == 0 || b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = (*get_op_func(sign))(a, b);
+
+	printf("%d\n", result);
+
+	return (0);
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Function entry
+ * Description: A function to add 2 integers
+ * @a: value to be added to b
+ * @b: value to add to a
+ * Return: added value
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Function entry
+ * Description: A function to sub 2 integers
+ * @a: value to be sub to b
+ * @b: value to sub to a
+ * Return: sub value
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Function entry
+ * Description: A function to multiply 2 integers
+ * @a: value to be multiply to b
+ * @b: value to multiply to a
+ * Return: multiplied value
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Function entry
+ * Description: A function to divide 2 integers
+ * @a: value to be divide to b
+ * @b: value to divide by a
+ * Return: added value
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Function entry
+ * Description: A function to modulo 2 integers
+ * @a: value to be find it modulo to b
+ * @b: value to divide to a
+ * Return: modulo value
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
