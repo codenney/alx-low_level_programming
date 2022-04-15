f43519954f0b326fbd23ca8a80e98f8c2aed1b33
Olaitan Adedokun <kenney9224@gmail.com>
2022-04-15 11:45:06 +0100

    Second to the last attempt!

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -30,7 +30,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", string);
 
-			if (separator && i < (n - 1))
+			if (separator == NULL && i < (n - 1))
 				printf("%s", separator);
 		}
 		i++;
