54b60dd05f940a2329d456f2c640091909ae3ae8
Olaitan Adedokun <kenney9224@gmail.com>
2022-04-16 19:38:41 +0100

    Include the function prototypes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,5 +8,16 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void _printString(va_list myList);
+void _printFloat(va_list myList);
+void _printInt(va_list myList);
+void _printChar(va_list myList);
+
+typedef struct converter
+{
+	char *type;
+	void (*func)();
+} converter;
+
 
 #endif
