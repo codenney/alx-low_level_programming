bb4edc039b86b86a4dbe635c114442cb7b9a829e
Olaitan Adedokun <kenney9224@gmail.com>
2022-04-26 06:48:18 +0100

    If index is zero

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newnode->n = n;
 
 	if (idx == 0)
-		return (add_nodeint(*head, n));
+		return (add_nodeint(head, n));
 
 	if (*head == NULL)
 	{
@@ -50,6 +50,31 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	newnode->next = temp->next;
 	temp->next =  newnode;
+
+	return (newnode);
+}
+
+
+/**
+ * add_nodeint - Function entry
+ * Description: A function that adds a new node
+ * ...at the beginning of a listint_t list
+ * @head: The pointer to the head address
+ * @n: value to add to the new node
+ * Return: the address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode;
+
+	newnode = (listint_t *)malloc(sizeof(listint_t));
+
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->n = n;
+	newnode->next = *head;
+	*head = newnode;
 
 	return (newnode);
 }
