6921a03cd5531d9e8052c2520397eb1fe9f37937
Olaitan <kenney9224@gmail.com>
2023-02-24 02:25:27 +0100

    removed my printf

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,8 +17,6 @@ void print_remaining_days(int month, int day, int year)
 	 */
 	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
-		printf("day ---%d\n", day);
-
 		if (month > 2 && day >= 60)
 		{
 			day++;
