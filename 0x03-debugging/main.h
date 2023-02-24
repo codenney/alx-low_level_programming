c1d416ab6d9f76b5ef2b5609f1c63605551a9aa9
Olaitan <kenney9224@gmail.com>
2023-02-24 01:56:44 +0100

    updated header file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -18,5 +18,7 @@ void print_to_98(int n);
 void positive_or_negative(int n);
 int largest_number(int a, int b, int c);
 
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif
