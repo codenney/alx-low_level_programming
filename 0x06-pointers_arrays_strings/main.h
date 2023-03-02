2185b2530684a03d619513777d5ff1bcb58dcfc3
Alvicci <kenney9224@gmail.com>
2023-03-02 02:30:46 +0100

    A custom strncpy function

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -10,6 +13,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *_strncpy(char *dest, char *src, int n);
 
 
 #endif
