41e2ea501e7df1c07a2ad3dfc1d29dbe428bf2ab
Alvicci <kenney9224@gmail.com>
2023-03-03 00:06:13 +0100

    check if the character is same

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,21 +3,33 @@
 /**
  * *cap_string - function entry
  * Description: A function that capitalizes all words of a string
- * Return:
+ * @ch: parameter
+ * Return: the array
  */
 char *cap_string(char *ch)
 {
-	int i = 0;
+	int i = 0, j;
+	char seperators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\0'};
 
 	while (ch[i] != '\0')
 	{
-		if ((ch[i] == ' ' || ch[i] == '\t' || ch[i] == '\n' || ch[i] == ',' ||
-			ch[i] == ';' || ch[i] == '.' || ch[i] == '!' || ch[i] == '?' ||
-			ch[i] == '"' || ch[i] == '(' || ch[i] == ')' || ch[i] == '{' ||
-			ch[i] == '}') 
-		&& (ch[i + 1] >= 'a' && ch[i + 1] <= 'z'))
+		if (ch[i] >= 97 && ch[i] <= 122)
 		{
-				ch[i + 1] = ch[i + 1] - 32;
+			if (i == 0)
+				ch[i] = ch[i] - 32;
+			else
+			{
+				j = 0;
+				while (seperators[j] != '\0')
+				{
+
+					if (ch[i - 1] == seperators[j])
+					{
+						ch[i] = ch[i] - 32;
+					}
+					j++;
+				}
+			}
 		}
 		i++;
 	}
