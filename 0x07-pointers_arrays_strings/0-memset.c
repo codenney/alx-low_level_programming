fbae53c29654378a7947a42c545592ba4bf98b72
Alvicci <kenney9224@gmail.com>
2023-03-06 14:38:07 +0100

    removed addition of the null condition

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * *_memset - function entry
+ * _memset - Function Entry
  * Description: A function that fills memory with a constant byte
- * @s: memory area pointed to
- * @b: fill s with the constant byte b
- * @n: bytes of the memory
- * Return: a pointer to the memory area s
+ * @s: starting address of memory
+ * @b: value to be filled
+ * @n: number of bytes to be filled
+ * Return: char*
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while (s[i] != '\0' || i < n)
+	while (i < n)
 	{
 		s[i] = b;
 		i++;
