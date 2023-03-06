90d4912a2125cadf15371c1f66a08ffb8ad2ceb0
Alvicci <kenney9224@gmail.com>
2023-03-06 14:36:39 +0100

    added if it reach the null character

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _memset - Function Entry
+ * *_memset - function entry
  * Description: A function that fills memory with a constant byte
- * @s: starting address of memory
- * @b: value to be filled
- * @n: number of bytes to be filled
- * Return: char*
+ * @s: memory area pointed to
+ * @b: fill s with the constant byte b
+ * @n: bytes of the memory
+ * Return: a pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while (i < n)
+	while (s[i] != '\0' || i < n)
 	{
 		s[i] = b;
 		i++;
