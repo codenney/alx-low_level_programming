6baa7328b368dbbec7d381742f88b9d52f84ef9f
Alvicci <kenney9224@gmail.com>
2023-03-21 17:57:05 +0100

    create a number code using the arrow sign

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,29 +1,31 @@
 #include "dog.h"
 
 /**
- * print_dog - Function entry
+ * print_dog - function entry
  * Description: A function that prints a struct dog
- * @d: The struct base address
+ * @d: address to a struct type
+ * If an element of d is NULL, print (nil) instead of this element.
+ * (if name is NULL, print Name: (nil))
+ * if d is NULL print nothing.
  * Return: See nothing!
  */
-
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
 
-	if ((*d).name == NULL)
+	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
-		printf("Name: %s\n", (*d).name);
+		printf("Name: %s\n", d->name);
 
-	if ((*d).age < 0)
+	if (d->age < 0.00)
 		printf("Age: (nil)\n");
 	else
-		printf("Age: %f\n", (*d).age);
+		printf("Age: %f\n", d->age);
 
-	if ((*d).owner == NULL)
+	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
-		printf("Owner: %s\n", (*d).owner);
+		printf("Owner: %s\n", d->owner);
 }
