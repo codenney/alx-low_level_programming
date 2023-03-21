c8f0d8ed27c261c335ff29e75cb2a73a6db4a9c7
Alvicci <kenney9224@gmail.com>
2023-03-21 17:40:37 +0100

    using the arrow sign to access struct elements

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,22 +1,20 @@
 #include "dog.h"
-#include <stdlib.h>
 
 /**
- * init_dog - Function entry
- * Description: A function that initialize a variable of type struct dog
- * @d: Base address of the struct type
- * @name: name character
- * @age: age holder
- * @owner: owner holder
- * Return: See nothing!!!
+ * init_dog - function entry
+ * Description: A unction that initialize a variable of type struct dog
+ * @d: address of the dig type
+ * @name: name of the dog
+ * @age: dog age
+ * @owner: dog owner
+ * Return: the land of nothing!
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		(*d).name = name;
-		(*d).age = age;
-		(*d).owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
-
 }
