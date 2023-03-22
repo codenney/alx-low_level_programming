3c183ee0956e33930caaa8cb78da54be80649dbe
Alvicci <kenney9224@gmail.com>
2023-03-22 05:43:50 +0100

    copied the string into memory and free if NULL

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+unsigned int _strlen(char *s);
 /**
  * *new_dog - function entry
  * Description: A function that creates a new dog
@@ -11,14 +11,52 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	unsigned int i, nameL, ownerL;
 	dog_t *ptr = malloc(sizeof(dog_t));
 
-	if (ptr == NULL)
+	if (ptr == NULL || age < 0 || !(name) || !(owner))
+	{
+		free(ptr);
 		return (NULL);
+	}
+
+	nameL = _strlen(name);
+	ownerL = _strlen(owner);
+
+	ptr->owner = malloc(ownerL + 1);
+	ptr->name = malloc(nameL + 1);
+
+	if (ptr->name == NULL || ptr->owner == NULL)
+	{
+		free(ptr);
+		free(ptr->name);
+		free(ptr->owner);
+	}
+
+	for (i = 0; name[i] != '\0'; i++)
+		ptr->name[i] = name[i];
+	ptr->name[i] = '\0';
+
+	for (i = 0; owner[i] != '\0'; i++)
+		ptr->owner[i] = owner[i];
+	ptr->owner[i] = '\0';
 
 	ptr->age = age;
-	ptr->name = name;
-	ptr->owner = owner;
 
 	return (ptr);
+}
+
+/**
+ * _strlen - function entry
+ * Description: A function that returns length of a string
+ * @s: a string address
+ * Return: the string length
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
 }
