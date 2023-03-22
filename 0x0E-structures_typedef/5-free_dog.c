a51e393c96d502e5770eac66329fd39a24792bc5
Alvicci <kenney9224@gmail.com>
2023-03-22 05:52:34 +0100

    refactor the code using arrows

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,17 @@
 #include "dog.h"
-#include <stdlib.h>
 
 /**
- * free_dog - Function entry
+ * free_dog - function entry
  * Description: A function that frees dogs
- * @d: The struct data type
+ * @d: the dog home
  * Return: See nothing!
  */
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
 	{
-		free(((*d).name));
-		free(((*d).owner));
+		free(d->name);
+		free(d->owner);
 		free(d);
 	}
 }
