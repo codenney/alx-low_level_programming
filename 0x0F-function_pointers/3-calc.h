28b9b004a48b8512b3c00e8d82934f05b75238d7
Alvicci <kenney9224@gmail.com>
2023-03-23 03:14:34 +0100

    fix betty style for the header

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -4,13 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int (*get_op_func(char *s))(int, int);
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
 /**
  * struct op - Struct op
  *
@@ -22,5 +15,12 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+
+int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 #endif /* CALC_H */
