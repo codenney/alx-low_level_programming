b2c3d075ac2c76e546094909f328c1f5ccec07f7
Olaitan <kenney9224@gmail.com>
2023-03-23 20:27:32 +0100

    fixed betty styles on the header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,17 +7,23 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+void print_all(const char *const format, ...);
 void _printString(va_list myList);
 void _printFloat(va_list myList);
 void _printInt(va_list myList);
 void _printChar(va_list myList);
 
+/**
+ * struct converter - a converter struct
+ * @type: first type
+ * @func: function
+ *
+ * Description: Define a new struct converter
+ */
 typedef struct converter
 {
 	char *type;
 	void (*func)();
 } converter;
-
 
 #endif
