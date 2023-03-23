daa43d4a34f56647f8f1a82d5f61604e28dc4d57
Alvicci <kenney9224@gmail.com>
2023-03-23 03:15:51 +0100

    A file that houses the arithmetic functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,11 +1,9 @@
-#include "3-calc.h"
-
 /**
- * op_add - Function entry
- * Description: A function to add 2 integers
+ * op_add - function entry
+ * Description: An addition function
  * @a: value to be added to b
- * @b: value to add to a
- * Return: added value
+ * @b: value to be added to a
+ * Return: the sum of a and b
  */
 int op_add(int a, int b)
 {
@@ -13,11 +11,11 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - Function entry
- * Description: A function to sub 2 integers
- * @a: value to be sub to b
- * @b: value to sub to a
- * Return: sub value
+ * op_sub - function entry
+ * Description: A subtraction function
+ * @a: value b is to be taken from
+ * @b: value to subtract from a
+ * Return: the difference of a and b
  */
 int op_sub(int a, int b)
 {
@@ -25,11 +23,11 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - Function entry
- * Description: A function to multiply 2 integers
- * @a: value to be multiply to b
- * @b: value to multiply to a
- * Return: multiplied value
+ * op_mul - function entry
+ * Description: A multiplication function
+ * @a: value to multiply with b
+ * @b: value to multiply with a
+ * Return: the product of a and b. Prototype
  */
 int op_mul(int a, int b)
 {
@@ -37,11 +35,11 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - Function entry
- * Description: A function to divide 2 integers
- * @a: value to be divide to b
- * @b: value to divide by a
- * Return: added value
+ * op_div - function entry
+ * Description: A division function
+ * @a: value to be divided by b
+ * @b: value a to be divided by
+ * Return: the result of the division of a by b
  */
 int op_div(int a, int b)
 {
@@ -49,12 +47,12 @@ int op_div(int a, int b)
 }
 
 /**
- * op_mod - Function entry
- * Description: A function to modulo 2 integers
- * @a: value to be find it modulo to b
- * @b: value to divide to a
- * Return: modulo value
- */
+ * op_mod - function entry
+ * Description: A remainder function
+ * @a: value to find remainder by b
+ * @b: value to find remainder by a
+ * Return: the remainder of the division of a by b
+*/
 int op_mod(int a, int b)
 {
 	return (a % b);
