d5bf8a3412d4bfff588cf23ffb31ef3d174808ff
Alvicci <kenney9224@gmail.com>
2023-03-23 03:08:09 +0100

    Refactor the code for the simple calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,17 +1,16 @@
 #include "3-calc.h"
 
 /**
- * main - Main entry
- * Description: A program to perform arithmetics
+ * main - main function entry
+ * Description: A program to perform basic arithmetics
  * @argc: commandline count
  * @argv: commandline argument(s)
  * Return: 0 as success
  */
 int main(int argc, char **argv)
 {
-	int a, b;
-	int result = 0;
-	char *sign = argv[2];
+	int a, b, result = 0;
+	char *operator = argv[2];
 
 	if (argc != 4)
 	{
@@ -19,25 +18,24 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	if (get_op_func(sign) == NULL || sign[1] != '\0')
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	a = atoi(*(argv + 1));
-	b = atoi(*(argv + 3));
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-	if ((*(sign + 0) == '/' || *(sign + 0) == '%') && (a == 0 || b == 0))
+	if ((operator[0] == '/' || operator[0] == '%') && (a == 0 || b == 0))
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
 
-	result = (*get_op_func(sign))(a, b);
+	result = (*get_op_func(operator))(a, b);
 
 	printf("%d\n", result);
 
 	return (0);
-
 }
