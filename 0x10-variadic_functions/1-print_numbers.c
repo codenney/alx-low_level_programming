12c398be7aebb2a0ea7bfc35597f7eb3f34aaeb7
Olaitan <kenney9224@gmail.com>
2023-03-23 20:54:00 +0100

    checked the condition and print only the string

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,10 +21,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(args, int);
+		printf("%d", num);
 		if (i < n - 1)
-			printf("%d%s", num, separator);
-		else
-			printf("%d", num);
+			printf("%s", separator);
 	}
 
 	printf("\n");
