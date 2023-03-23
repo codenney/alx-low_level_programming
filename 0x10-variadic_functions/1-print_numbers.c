241a5e21920daf398236a44d7463655bf785a140
Olaitan <kenney9224@gmail.com>
2023-03-23 20:59:19 +0100

    changed to while loop

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,32 +1,30 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - function entry
- * Description:
+ * print_numbers - Function entry
+ * Description: A function that prints numbers, followed by a new line
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
- * Return: nothing!
+ * @...: list of arguments
+ * Return: See nothing!
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	int num;
-	va_list args;
+	va_list myarg;
 
-	if (separator == NULL)
-		return;
+	va_start(myarg, n);
 
-	va_start(args, n);
-
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
-		num = va_arg(args, int);
+		num = va_arg(myarg, int);
 		printf("%d", num);
 		if (separator && i < (n - 1))
 			printf("%s", separator);
+		i++;
 	}
-
 	printf("\n");
 
-	va_end(args);
+	va_end(myarg);
 }
