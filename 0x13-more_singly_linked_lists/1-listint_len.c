1c02c4fae32d0dfab0896d36610fee9a1f35ab1b
Alvicci <kenney9224@gmail.com>
2023-04-05 06:52:58 +0100

    A function that returns the number of elements

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 
 /**
- * listint_len - Function entry
+ * listint_len - function entry
  * Description: A function that returns the number of elements
  * in a linked listint_t list
- * @h: address of the head node
+ * @h: the address of the head node
  * Return: number of elements
  */
 size_t listint_len(const listint_t *h)
 {
-	unsigned int i = 0;
+	unsigned int count = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		i++;
+		count++;
 	}
-	return (i);
+
+	return (count);
 }
