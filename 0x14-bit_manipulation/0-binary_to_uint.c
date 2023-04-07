d0f971edd67df2658837bf5ed7bb6db855d70f6f
Alvicci <kenney9224@gmail.com>
2023-04-07 07:29:36 +0100

    A function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,30 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Function entry
+ * binary_to_uint -  function entry
  * Description: A function that converts a binary number to an unsigned int
- * @b: is pointing to a string of 0 and 1 chars
- * Return: the converted number, or 0 if...
- * there is one or more chars in the string b that is not 0 or 1
- * b is NULL
+ * @b: the binary digit to convert
+ * Return: the converted number, or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum = 0;
-	int bitlen = 0, bin = 1, convert = 0;
+	int arrLength, convertToDigit, increaseBinary = 1, decimal = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[bitlen] != '\0')
-	{
-		bitlen++;
-	}
+	for (arrLength = 0; b[arrLength] != '\0'; arrLength++)
+		;
 
-	bitlen--;
-	while (bitlen >= 0)
+	for (arrLength -= 1; arrLength >= 0; arrLength--)
 	{
-		if (b[bitlen] != '0' && b[bitlen] != '1')
+		if (b[arrLength] != '1' && b[arrLength] != '0')
 			return (0);
 
-		convert = b[bitlen] - '0';
-		sum += convert * bin;
-		bin *= 2;
-
-		bitlen--;
+		convertToDigit = b[arrLength] - '0';
+		decimal += convertToDigit * increaseBinary;
+		increaseBinary *= 2;
 	}
-	return (sum);
+
+	return (decimal);
 }
